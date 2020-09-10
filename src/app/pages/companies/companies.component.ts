a6b7a734d9e6136663f9b731120fe3d00bf1649d
baberarjumand <baber.arjumand@gmail.com>
2020-09-10 19:25:12 +0300

    able to load and display data from mockapi

--- src/app/pages/companies/companies.component.ts
+++ src/app/pages/companies/companies.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { CompanyService } from './../../shared/services/company.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Company } from 'src/app/shared/model/company';
+import { Subscription, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.component.html',
   styleUrls: ['./companies.component.scss'],
 })
-export class CompaniesComponent implements OnInit {
-  searchString: string;
+export class CompaniesComponent implements OnInit, OnDestroy {
+  // searchString: string;
+  searchForm: FormGroup;
+  searchResults: Company[];
+  searchResultsSub: Subscription;
 
-  constructor() {}
+  // searchResultSet = new BehaviorSubject<Company[]>(null);
+
+  constructor(private companyService: CompanyService) {}
 
   ngOnInit(): void {
-    this.searchString = '';
+    this.searchResultsSub = this.companyService
+      .getTenCompanies()
+      .subscribe((companies) => {
+        this.searchResults = companies;
+        // console.log(this.searchResults);
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.searchResultsSub) {
+      this.searchResultsSub.unsubscribe();
+    }
   }
 
   onSearchbarClick(): void {}
--- src/app/shared/services/company.service.ts
+++ src/app/shared/services/company.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Company } from '../model/company';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CompanyService {
+  COMPANIES_URL = 'https://5f59cabb8040620016ab960d.mockapi.io/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllCompanies(): Observable<Company[]> {
+    return this.http.get<Company[]>(this.COMPANIES_URL + 'companies');
+  }
+
+  private getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
+
+  getTenCompanies(): Observable<Company[]> {
+    const randomInt = this.getRandomIntInclusive(1, 5);
+    return this.http.get<Company[]>(
+      this.COMPANIES_URL + 'companies' + '?page=' + randomInt + '&limit=10'
+    );
+  }
 }
